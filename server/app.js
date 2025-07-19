f92412b2a1bd4976e5f125578ec8d384a2976ce2
Vicky1jat <vicky1jat6376831267@gmail.com>
2025-07-19 11:44:07 +0530

    made chnages in routes

--- server/app.js
+++ server/app.js
@@ -1,17 +1,18 @@
 // server/index.js
 import express from 'express';
 import cors from 'cors';
-import { askGPT } from './controllers/devbuddyController.js';
-import { runCode } from './controllers/judge0Controller.js';
+import 'dotenv/config'; 
+
+import router from './routes/index.js';
 
 const app = express();
 app.use(cors()); // important for local dev
 app.use(express.json());
 
 // API route
-app.post('/api/ask', askGPT);
+app.use('/api', router);
 
-app.post('/api/run-code', runCode);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import { askGPT } from '../controllers/devbuddyController.js';
-
+import { runCode } from '../controllers/judge0Controller.js';
 const router = express.Router();
+
 router.post('/ask', askGPT);
+
+router.post('/run-code', runCode);
 
 export default router;
